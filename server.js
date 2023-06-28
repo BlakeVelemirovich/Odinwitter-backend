87d8f2f06cf931a0094e1b3996727b2ad77cb9ee
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-06-28 16:42:34 -0300

    Models and routes added

--- server.js
+++ server.js
@@ -3,23 +3,26 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3001;
-const message_controller = require('./controllers/message');
+const message_controller = require('./controllers/message_controller');
+const user_controller = require('./controllers/user_controller');
 
 // database connection setup using a secret key
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = MONGODB_CONNECTION_URL;
+const mongoDB = process.env.MONGODB_CONNECTION_URL;
 
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
-}
+}*/
 
 app.use(express.json());
 app.use(cors());
 
 // route for when a user submits a new post
 app.post('/api/strike', message_controller.new_message);
+
+app.post('/api/signup', user_controller.signup);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
